f8e231138d89b4a71da7b223b51eb33a7d1e3617
Saymed <gasayagom@gmail.com>
2023-01-04 21:24:31 -0400

    Finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        string ans = "";
+        do{
+            int number = randomGenerator.Next(1, 100);
+            int guess, times = 0;
+            bool flag = true;
+
+            while(flag){
+
+                Console.WriteLine("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if(guess > number) Console.WriteLine("Lower");
+                else if(guess < number) Console.WriteLine("Higher");
+                else flag = false;
+                times++;
+            }
+
+            Console.WriteLine("You guessed it in {0} times!\n\nDo you want to play again? (yes/no)",times);
+            ans = Console.ReadLine();
+        }while(ans.Equals("yes") || ans.Equals("y"));
+
     }
 }
