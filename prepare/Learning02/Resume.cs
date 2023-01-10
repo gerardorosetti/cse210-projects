fb689320931d5a4e0a09a8faffa8ea68325bdedf
Saymed <gasayagom@gmail.com>
2023-01-10 16:12:27 -0400

    Finished Learning02

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,10 @@
+public class Resume {
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+    public void Display(){
+        Console.WriteLine("Name: {0}\nJobs:",_name);
+        foreach(Job _job in _jobs){
+            _job.Display();
+        }
+    }
+}
