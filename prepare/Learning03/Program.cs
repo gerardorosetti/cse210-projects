9ade8645a08031c6dde97a67232ea1b7e5141612
Saymed <gasayagom@gmail.com>
2023-01-19 14:27:00 -0400

    Finished Learning 3

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine($"{fraction1.GetFractionString()}\n{fraction1.GetDeciamlValue()}");
+
+        Fraction fraction2 = new Fraction(5);
+        Console.WriteLine($"{fraction2.GetFractionString()}\n{fraction2.GetDeciamlValue()}");
+
+        Fraction fraction3 = new Fraction(3,4);
+        Console.WriteLine($"{fraction3.GetFractionString()}\n{fraction3.GetDeciamlValue()}");
+
+        fraction3.SetTop(1);
+        fraction3.SetBottom(3);
+        Console.WriteLine($"{fraction3.GetFractionString()}\n{fraction3.GetDeciamlValue()}");
     }
 }
