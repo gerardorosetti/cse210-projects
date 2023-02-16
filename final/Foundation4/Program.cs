e98e0f359417a80396b2b22b319c153c32e93511
Saymed <gasayagom@gmail.com>
2023-02-16 13:41:10 -0400

    Finished Foundation4

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        Console.Clear();
+        List<Activity> activities =  new List<Activity>();
+        activities.Add(new Running("03 Nov 2022",4.8,30));
+        activities.Add(new Cycling("05 Nov 2022",9.6,30));
+        activities.Add(new Swimming("08 Nov 2022",96,30));
+        int i = 1;
+        foreach(Activity activity in activities)
+        {
+            Console.WriteLine($"\nFor activity number {i}:\n{activity.GetSummary()}");
+            i++;
+        }
+        Console.WriteLine();
     }
 }
